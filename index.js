b795f498e2fc9793b8e3de956fe15602b1549660
Chris Mumford <cmumford@cmumford.com>
2025-03-14 15:10:29 -0400

    Cleanup response

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 80;
 
+const date = new Date();
+const response = {
+    "message": "My name is timestamper",
+    "timestamp": date.valueOf()
+};
+
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js in Docker!');
+    res.send(JSON.stringify(response));
 });
 
 app.listen(PORT, () => {
