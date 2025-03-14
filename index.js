72bd58135be81507ffc9e325e8e3fba241cb29ab
Chris Mumford <cmumford@cmumford.com>
2025-03-14 15:16:00 -0400

    Return JSON response (not text)

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 80;
-
 const date = new Date();
-const response = {
-    "message": "My name is timestamper",
-    "timestamp": date.valueOf()
-};
 
 app.get('/', (req, res) => {
-    res.send(JSON.stringify(response));
+    const response = {
+        "message": "My name is timestamper",
+        "timestamp": date.valueOf() + 2 * 1000
+    };
+    res.send(response);
 });
 
 app.listen(PORT, () => {
