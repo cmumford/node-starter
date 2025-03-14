58b632de7cd64d53f1ff1f4eeb10b1d395592121
Chris Mumford <cmumford@cmumford.com>
2025-03-14 15:26:39 -0400

    Move scope of Date object

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 80;
-const date = new Date();
 
 app.get('/', (req, res) => {
+    const now = new Date();
     const response = {
         "message": "My name is timestamper",
-        "timestamp": date.valueOf() + 7 * 1000
+        "timestamp": now.valueOf() + 7 * 1000
     };
     res.send(response);
 });
