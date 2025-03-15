2f28f59412c299987be43fa674bf570075312b9d
Chris Mumford <cmumford@cmumford.com>
2025-03-15 08:16:30 -0400

    Remove negative time offset

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const PORT = 80;
 
 app.get('/', (req, res) => {
     const now = new Date();
-    const response = {
+    res.send({
         "message": "My name is timestamper",
-        "timestamp": now.valueOf() - 1 * 1000
-    };
-    res.send(response);
+        "timestamp": now.valueOf()
+    });
 });
 
 app.listen(PORT, () => {
