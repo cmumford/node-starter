7b72b8680b4019b7674acf4bd910c2bb47c1c385
Chris Mumford <cmumford@cmumford.com>
2025-03-20 11:52:40 -0400

    Remove string comparison from isJsonMinified
    Comparing the two strings for equality is unnecessary because
    had they not matched then we consider equal lengths to be
    minimized. So, if equality ensures equal length, then just
    compare lengths as the first check is redundant.

--- test/app.test.mjs
+++ test/app.test.mjs
@@ -5,29 +5,38 @@ const server = app;
 
 const dockerBridgeIP = "127.0.0.1";
 
+// Determine if a valid JSON string is minified. Minified typically means
+// (at a minimum) the elinination of all whitespace.
 function isJsonMinified(jsonString) {
     try {
-        const jsonObj = JSON.parse(jsonString);
-        const minified = JSON.stringify(jsonObj);
-        if (jsonString === minified) {
-            return true;
-        } else {
-            // JSON.stringify does not guarantee the stable order of keys.
-            // Furthermore JSON created by other means (Go, Rust, ...) may have
-            // no (or different) guarantees of stability. For example an object
-            // is converted to the JSON string '{"b":2,"a":1}' by one program
-            // may be converted to '{"a":1,"b":2}' in another. Comparing the
-            // length is a workaround for this. It won't work for "relaxed"
-            // JSON, but it will for "strict" JSON.
-            return minified.length == jsonString.length;
-        }
+        const minified = JSON.stringify(JSON.parse(jsonString));
+        // JSON.stringify does not guarantee the stable order of keys.
+        // Furthermore JSON created by other means (Go, Rust, ...) may have
+        // no (or different) guarantees of stability. For example an object
+        // is converted to the JSON string '{"b":2,"a":1}' by one program
+        // may be converted to '{"a":1,"b":2}' in another. Comparing the
+        // length is a workaround for this. It won't work for "relaxed"
+        // JSON, but it will for "strict" JSON.
+        return minified.length == jsonString.length;
     } catch (error) {
-        console.error(error);
         return false;
     }
 }
 
 describe("Tests to the \"/\" endpoint", () => {
+    it("shall be minimized ('a' key first)", async () => {
+        expect(isJsonMinified('{"a":1,"b":2}')).to.be.true;
+    });
+    it("shall be minimized ('a' key last)", async () => {
+        expect(isJsonMinified('{"b":2,"a":1}')).to.be.true;
+    });
+    it("is not minimized", async () => {
+        expect(isJsonMinified('{"b":2, "a":1}')).to.be.false;
+    });
+    it("Invalid JSON", async () => {
+        expect(isJsonMinified('{"b"')).to.be.false;
+    });
+
     it("should return a 200 status code", async () => {
         const res = await axios(`http://${dockerBridgeIP}:3000/`);
         expect(res.status).to.equal(200);
