431b5f4a772eedbbbed59064296e5231d329cc56
Chris Mumford <cmumford@cmumford.com>
2025-03-20 10:37:08 -0400

    Run tests on port 3000
    The tests were failing on GitHub with the following error:
    
    ```
    Uncaught Error: listen EACCES: permission denied 0.0.0.0:80
    ```
    
    due to a permissions error. Add a PORT environment variable,
    which defaults to 80, so that the tests can use a non-privileged
    port.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:lts-alpine
 ARG GIT_SHA1=prod
 ENV NODE_ENV=production
 ENV GIT_COMMIT_ID=$GIT_SHA1
+ENV PORT=80
 WORKDIR /usr/src/node-starter
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install --production --silent && mv node_modules ../
--- test/app.test.js
+++ test/app.test.js
@@ -28,36 +28,36 @@ function isJsonMinified(jsonString) {
 
 describe("Tests to the \"/\" endpoint", () => {
     it("should return a 200 status code", async () => {
-        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        const res = await axios(`http://${dockerBridgeIP}:3000/`);
         expect(res.status).to.equal(200);
     });
 
     it("should return a JSON object with a Message", async () => {
-        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        const res = await axios(`http://${dockerBridgeIP}:3000/`);
         expect(res.data).to.haveOwnProperty("message");
     });
 
     it("should return a JSON object with a Timestamp", async () => {
-        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        const res = await axios(`http://${dockerBridgeIP}:3000/`);
         expect(res.data).to.haveOwnProperty("timestamp");
     });
 
     it("should return a Message saying \"My name is ...\"", async () => {
-        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        const res = await axios(`http://${dockerBridgeIP}:3000/`);
         expect(res.data.message).to.contain("My name is");
     });
 
     it("should return a UNIX style timestamp (numerical values only)", async () => {
-        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        const res = await axios(`http://${dockerBridgeIP}:3000/`);
         expect(res.data.timestamp).to.be.a("number");
     });
     it("should return a timestamp within a few seconds of now", async () => {
-        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        const res = await axios(`http://${dockerBridgeIP}:3000/`);
         const now = Date.now();
         expect(res.data.timestamp).to.be.within(now - 5000, now);
     });
     it("should return minified JSON", async () => {
-        const res = await axios(`http://${dockerBridgeIP}:80/`, { responseType: 'text' });
+        const res = await axios(`http://${dockerBridgeIP}:3000/`, { responseType: 'text' });
         expect(res.headers["content-type"]).to.contain("application/json");
         expect(isJsonMinified(res.data)).to.be.true;
     });
